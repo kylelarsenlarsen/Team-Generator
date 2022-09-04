b79e7c698fd6e4d5eddf3446a43946c8c8ae81c2
Kyle Larsen <kylelarsen.business@gmail.com>
2022-09-04 11:51:16 -0600

    reverted back to a previous version of jest package. created my first unit test for the employee class.

---
+++ tests/Employee.test.js
@@ -0,0 +1,16 @@
+const testEmployee = require('../lib/Employee')
+
+describe('testEmployee', () => {
+    describe('employee', () => {
+        it('The Employee parameters should match the constants.', () => {
+            const name = 'Test Name';
+            const id = 97679869;
+            const email = 'testexample@email.com';
+            const employee = new testEmployee('Test Name', 97679769, 'testexample@email.com')
+            expect(employee.name).toEqual(name)
+            expect(employee.id).toEqual(id)
+            expect(employee.email).toEqual(email)
+            expect(employee.getRole()).toEqual('Employee')
+        })
+    })
+})
