e82ae1a7f00fd3e8769f6a91c18e4f073f9354ed
Kyle Larsen <kylelarsen.business@gmail.com>
2022-09-11 22:37:08 -0600

    got rid of the error catch function. inserted new functions to handle the main boilerplate html to take the cards generated from the prompt.

--- src/HTML.js
+++ src/HTML.js
@@ -4,77 +4,87 @@ const theGeneratedTeam = (myTeam) => {
     let populateManager = manager => {
         let managerSegment = `<section class="card" style="width: 350px">
         <div class="card-header" style="background: rgba(11, 74, 134, 0.856); color: white;">
-        <h3 class="card-title">${manager.nameofmanager}</h3>
+        <h3 class="card-title">${manager.name}</h3>
         <h4 class="card-title"><i class="fa-solid fa-mug-saucer"></i>Manager</h4>
         </div>
         <div class="card-body" style="background:rgb(250, 250, 255)">
         <ul class="list-group">
-            <li class="list-group-item">ID: ${manager.idofmanager}</li>
-            <li class="list-group-item">Email: ${manager.emailofmanager}</li>
-            <li class="list-group-item">Office Number: ${manager.officenumberofmanager}</li>
+            <li class="list-group-item">ID: ${manager.id}</li>
+            <li class="list-group-item">Email: ${manager.email}</li>
+            <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
         </ul>
     </div>
     </section>`;
-    generatedTeam.push(managerSegment);
+        generatedTeam.push(managerSegment);
     }
-    
+
     let populateEngineer = engineer => {
         let engineerSegment = `<section class="card" style="width: 350px">
         <div class="card-header" style="background: rgba(11, 74, 134, 0.856); color: white;">
-        <h3 class="card-title">INSERT NAME</h3>
+        <h3 class="card-title">${engineer.name}</h3>
         <h4 class="card-title"><i class="fa-solid fa-wrench"></i>Engineer</h4>
         </div>
         <div class="card-body" style="background:rgb(250, 250, 255)">
             <ul class="list-group">
-                <li class="list-group-item">ID:</li>
-                <li class="list-group-item">Email:</li>
-                <li class="list-group-item">Office Number:</li>
+                <li class="list-group-item">ID: ${}</li>
+                <li class="list-group-item">Email: ${}</li>
+                <li class="list-group-item">School: ${}</li>
             </ul>
         </div>
     </section>`;
-    generatedTeam.push(engineerSegment);
+        generatedTeam.push(engineerSegment);
     }
 
     let populateIntern = intern => {
         let internSegment = `<section class="card" style="width: 350px">
         <div class="card-header" style="background: rgba(11, 74, 134, 0.856); color: white;">
-        <h3 class="card-title">INSERT NAME</h3>
+        <h3 class="card-title">${}</h3>
         <h4 class="card-title"><i class="fa-solid fa-graduation-cap"></i>Intern</h4>
         </div>
         <div class="card-body" style="background:rgb(250, 250, 255)">
             <ul class="list-group">
-                <li class="list-group-item">ID:</li>
-                <li class="list-group-item">Email:</li>
-                <li class="list-group-item">Office Number:</li>
+                <li class="list-group-item">ID: ${}</li>
+                <li class="list-group-item">Email: ${}</li>
+                <li class="list-group-item">School: ${}</li>
             </ul>
         </div>
     </section>`;
-    generatedTeam.push(internSegment);
-    }
-
-    for(const bodySegment of generatedTeam) {`<!DOCTYPE html>
-
-        <html>
-        
-        <head>
-            <meta charset="utf-8">
-            <meta http-equiv="X-UA-Compatible" content="IE=edge">
-            <title>Team Generator</title>
-            <meta name="description" content="">
-            <meta name="viewport" content="width=device-width, initial-scale=1">
-            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-                integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-            <script src="https://kit.fontawesome.com/78a2821f83.js" crossorigin="anonymous"></script>
-        </head>
-        
-        <body>
-        ${bodySegment}
-        </body>
-
-        </html>`
+        generatedTeam.push(internSegment);
     }
 }
 
-fs.writeFile('index.html', generatedTeam, (err) =>
-      err ? console.log(err.message) : console.log('Successfully created index.html!')
-    );
+
+function renderHTML(myTeam) {
+    return `<!DOCTYPE html>
+
+    <html>
+    
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Team Generator</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+        <script src="https://kit.fontawesome.com/78a2821f83.js" crossorigin="anonymous"></script>
+    </head>
+    
+    <body>
+
+    <nav class="text-center" style="background:rgba(233, 83, 14, 0.76)">
+    <p class="fs-1 text-white">Your Team</p>
+    </nav>
+
+    <div class="team justify-content-center d-flex flex-wrap" style="margin-top: 50px; gap: 20px;">
+
+    ${theGeneratedTeam(myTeam)}
+
+    </div>
+
+    </body>
+
+    </html>`
+}
+
+module.exports = renderHTML
